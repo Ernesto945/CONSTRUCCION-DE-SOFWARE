734f036749732f0f34ee8152cd021941d55508ca
Ernesto945 <141592915+Ernesto945@users.noreply.github.com>
2024-12-03 19:47:20 -0500

    Update borrarvariable.php

--- test/borrarvariable.php
+++ test/borrarvariable.php
@@ -16,14 +16,17 @@ session_destroy();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Borrar Variables</title>
     <link rel="stylesheet" href="style.css">
+    <script>
+        setTimeout(function() {
+            window.location.href = '/sistema/login.php'; // Redirige a login después de 3 segundos
+        }, 3000);
+    </script>
 </head>
 <body>
     <div class="container">
         <h1>Se borraron todas las variables</h1>
         <p>Las variables de sesión se han eliminado y la sesión se ha destruido.</p>
-        <br>
-        <a href="http://127.0.0.1/sistema/vervariables.php" class="btn">Ver Variables</a>
-        <a href="http://127.0.0.1/sistema/test.php" class="btn">Volver a grabar las variables</a>
+        <p>Serás redirigido al inicio en 3 segundos...</p>
     </div>
 </body>
 </html>
