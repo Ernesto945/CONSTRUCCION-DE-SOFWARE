8c291aeca2657369c01d2b95e2f30c5e148aa425
Ernesto945 <141592915+Ernesto945@users.noreply.github.com>
2024-12-03 19:38:27 -0500

    Update config.php

--- etc/config.php
+++ etc/config.php
@@ -1,27 +1,36 @@
 <?php
 
+// Base URL for the application
 $_urlBase = "http://MC_CONSTRUCION_SW.test/";
+
+// Database connection details (consider storing these in environment variables for security)
 $host = '127.0.0.1';
 $namedb = 'dbsistema';
 $userdb = 'root';
-$passwordb = '';
+$passwordb = ''; // Ensure secure password handling in production
 
+// Function to get the base URL
 function get_UrlBase($arg1)
 {
     return $GLOBALS['_urlBase'] . $arg1;
 }
 
+// Function to get the URL for views
 function get_views($arg1)
 {
     return $GLOBALS['_urlBase'] . 'views/' . $arg1;
 }
 
+// Function to get the URL for models
 function get_models($arg1)
 {
     return $GLOBALS['_urlBase'] . 'models/' . $arg1;
 }
+
+// Function to get the URL for controllers
 function get_controllers($arg1)
 {
     return $GLOBALS['_urlBase'] . 'controllers/' . $arg1;
 }
+
 ?>
