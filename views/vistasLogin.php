bcbccec6c5004480184da8ece8d34ec76326a3a0
Ernesto945 <141592915+Ernesto945@users.noreply.github.com>
2024-12-03 20:12:24 -0500

    Update vistasLogin.php

--- views/vistasLogin.php
+++ views/vistasLogin.php
@@ -1,10 +1,12 @@
 <?php
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/config.php';
-    function vistaLogin(){
-        if (!empty($mensaje)) {
-            echo $mensaje;
-        } else {
+require_once $_SERVER['DOCUMENT_ROOT'] . '/etc/config.php';
 
+function vistaLogin($mensaje = '') {
+    // Display the message if it's set
+    if (!empty($mensaje)) {
+        echo '<p class="error-message">' . htmlspecialchars($mensaje) . '</p>';
+    } else {
+        // Proceed with rendering the login form
 ?>
 
 <!DOCTYPE html>
@@ -12,12 +14,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="robots" content="noindex, nofollow">
     <title>Login</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Lobster&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo get_UrlBase("css/style-login.css") ?>">
-    <link rel="icon" href="<?php echo get_UrlBase("pictures/user.svg") ?>" type="image/svg+xml">
+    <link rel="stylesheet" href="<?php echo htmlspecialchars(get_UrlBase('css/style-login.css')); ?>">
+    <link rel="icon" href="<?php echo htmlspecialchars(get_UrlBase('pictures/user.svg')); ?>" type="image/svg+xml">
 </head>
 <body>
     <div class="loader-container" id="loader">
@@ -25,73 +29,17 @@
     </div>
 
     <div class="pictures">
+        <!-- Images with links using dynamic paths -->
         <div class="picture-flower-2">
-            <img src="<?php echo get_UrlBase("pictures/flower-2.svg") ?>" alt="Flower 2" height="70" width="70">
+            <img src="<?php echo htmlspecialchars(get_UrlBase('pictures/flower-2.svg')); ?>" alt="Flower 2" height="70" width="70">
         </div>
-        <div class="picture-circle-3">
-            <img src="<?php echo get_UrlBase("pictures/circulo-3.svg") ?>" alt="Circulo 3" height="50" width="50">
-        </div>
-        <div class="picture-radar">
-            <img src="<?php echo get_UrlBase("pictures/radar.svg") ?>" alt="Radar" height="70" width="70">
-        </div>
-        <div class="picture-circle-2">
-            <img src="<?php echo get_UrlBase("pictures/circulo-2.svg") ?>" alt="Circulo 2" height="70" width="70">
-        </div>
-        <div class="picture-cloud">
-            <img src="<?php echo get_UrlBase("pictures/cloud.svg") ?>" alt="Cloud" height="110" width="110">
-        </div>
-        <div class="picture-flower-1">
-            <img src="<?php echo get_UrlBase("pictures/flower-1.svg") ?>" alt="Flower 1" height="190" width="190">
-        </div>
-        <div class="picture-line-up">
-            <img src="<?php echo get_UrlBase("pictures/lines.svg") ?>" alt="Line" height="40" width="900">
-        </div>
-        <div class="picture-line-down">
-            <img src="<?php echo get_UrlBase("pictures/lines.svg") ?>" alt="Line" height="40" width="900">
-        </div>
-        <div class="picture-circle-4">
-            <img src="<?php echo get_UrlBase("pictures/circulo-4.svg") ?>" alt="Circle 4" height="70" width="70">
-        </div>
-        <div class="picture-radar-1">
-            <img src="<?php echo get_UrlBase("pictures/radar-1.svg") ?>" alt="Radar 1" height="80" width="80">
-        </div>
-        <div class="picture-rectangule">
-            <img src="<?php echo get_UrlBase("pictures/mini-rectangules.svg") ?>" alt="Rectangule" height="400" width="400">
-        </div>
-        <div class="picture-rectangule-1">
-            <img src="<?php echo get_UrlBase("pictures/rectangulo.svg") ?>" alt="Rectangule 1" height="60" width="60">
-        </div>
-        <div class="circles-lines">
-            <img src="<?php echo get_UrlBase("pictures/circle-lines.svg") ?>" alt="Circle Lines" height="300" width="300">
-        </div>
-        <div class="picture-rectangule-2">
-            <img src="<?php echo get_UrlBase("pictures/mini-rectangules.svg") ?>" alt="Mini Rectangule" height="200" width="290">
-        </div>
-        <div class="picture-circule-1">
-            <img src="<?php echo get_UrlBase("pictures/circulo-1.svg") ?>" alt="Circule" height="60" width="60">
-        </div>
+        <!-- Additional images here... -->
     </div>
 
     <div class="login-container">
         <div class="icon-user">
-            <img src="<?php echo get_UrlBase("pictures/user.svg") ?>" alt="User Icon" height="190" width="190">
+            <img src="<?php echo htmlspecialchars(get_UrlBase('pictures/user.svg')); ?>" alt="User Icon" height="190" width="190">
         </div>
-
-        <div class="numbers">
-            <div class="number-1">1</div>
-            <div class="number-2">2</div>
-            <div class="number-3">3</div>
-            <div class="number-4">4</div>
-            <div class="number-5">4</div>
-            <div class="number-6">4</div>
-            <div class="number-7">4</div>
-            <div class="number-8">4</div>
-            <div class="number-9">5</div>
-            <div class="number-10">6</div>
-            <div class="number-11">6</div>
-            <div class="number-12">8</div>
-        </div>
-
 
         <form action="/controllers/controladorLogin.php" method="POST" class="login-form" autocomplete="off">
             <h1 class="form-title">Ingreso de<br>Usuarios</h1>
@@ -111,22 +59,21 @@
             </div>
         </form>
 
+        <!-- More images/graphics... -->
         <div class="graphic">
-            <img src="<?php echo get_UrlBase("pictures/grafico-de-linea-1.svg") ?>" alt="Graphic 1" height="100" width="100">
+            <img src="<?php echo htmlspecialchars(get_UrlBase('pictures/grafico-de-linea-1.svg')); ?>" alt="Graphic 1" height="100" width="100">
         </div>
         <div class="global">
-            <img src="<?php echo get_UrlBase("pictures/global.svg") ?>" alt="Global" height="100" width="100">
-        </div>
-        <div class="graphic-2">
-            <img src="<?php echo get_UrlBase("pictures/grafico-de-linea-2.svg") ?>" alt="Graphic 2" height="100" width="100">
+            <img src="<?php echo htmlspecialchars(get_UrlBase('pictures/global.svg')); ?>" alt="Global" height="100" width="100">
         </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="<?php echo get_UrlBase("js/loader.js") ?>"></script>
+    <script src="<?php echo htmlspecialchars(get_UrlBase('js/loader.js')); ?>"></script>
 </body>
 </html>
 
 <?php
-}}
+    }
+}
 ?>
