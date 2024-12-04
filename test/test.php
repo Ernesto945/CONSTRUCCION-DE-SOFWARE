d9b3cebb05108065f24295f8cee9de51733f81d0
Ernesto945 <141592915+Ernesto945@users.noreply.github.com>
2024-12-03 19:48:46 -0500

    Update test.php

--- test/test.php
+++ test/test.php
@@ -1,21 +1,29 @@
 <?php
 // Start the session
 session_start();
+
+// Set session variables only if they are not already set
+if (!isset($_SESSION["favcolor"])) {
+    $_SESSION["favcolor"] = "green";
+}
+if (!isset($_SESSION["favanimal"])) {
+    $_SESSION["favanimal"] = "cat";
+}
+
+// Notify the user that the session variables have been set
+echo "Las variables ya han sido almacenadas .....";
+
+// Redirect to 'vervariables.php' after a short delay
+header("Refresh: 2; url=http://127.0.0.1/sistema/vervariables.php");
 ?>
+
 <!DOCTYPE html>
 <html>
 <body>
 
-<?php
-// Set session variables
-$_SESSION["favcolor"] = "green";
-$_SESSION["favanimal"] = "cat";
-echo "las variables ya han sido almacenadas .....";
-?>
-
+<!-- Provide a link to view session variables -->
 <br>
-
-<a href="http://127.0.0.1/sistema/vervariables.php"> ir a ver las variables </a>
+<a href="http://127.0.0.1/sistema/vervariables.php">Ir a ver las variables</a>
 
 </body>
 </html>
